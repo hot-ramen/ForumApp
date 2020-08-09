9b1c799ef46b382b2ab28502e4b1adb359196ef0
Ram <67579652+hot-ramen@users.noreply.github.com>
2020-08-09 14:58:52 +0800

    added code to MainActivity and Home

--- app/src/main/java/hotramen/apps/projectcs/Home.java
+++ app/src/main/java/hotramen/apps/projectcs/Home.java
@@ -1,17 +1,86 @@
 package hotramen.apps.projectcs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+import io.realm.Sort;
 
 @EActivity(R.layout.home)
 public class Home extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+    @ViewById
+    RecyclerView rvFeed;
+
+    @ViewById
+    Button btnMakeTrd;
+
+    @ViewById
+    Button btnEditProf;
+
+    @ViewById
+    Button btnSignOut;
+
+
+    Realm realm;
+
+
+    @AfterViews
+    public void init() {
+//        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+//        mLayoutManager.setOrientation(RecyclerView.VERTICAL);
+//        rvFeed.setLayoutManager(mLayoutManager);
+//
+//        realm = Realm.getDefaultInstance();
+//
+//        RealmResults<User> list = realm.where(User.class)
+//                .findAll()
+//                .sort("name", Sort.ASCENDING);
+//
+//        adapter = new UserAdapter(this, list, true);
+//        rvFeed.setAdapter(adapter);
+
+        //spaghetti code pa to copy past lang for init
+    }
+
+    @Click(R.id.btnSignOut)
+    public void close() {
+        finish();
+    }
+
+
+    public void delete(View view) {
+//        User user = (User) view.getTag();
+//
+//        // once an object is deleted you cant delete it again, check if isValid
+//        // this is needed since you can double tap a button very easily
+//        if (user.isValid())
+//        {
+//            realm.beginTransaction();
+//            user.deleteFromRealm();
+//            realm.commitTransaction();
+//        }
+    }
+
+    public void edit(View view) {
+//        User userToEdit = (User) view.getTag();
+//        String id = userToEdit.getUuid();
+//
+//
+//        EditActivity_.intent(this)
+//                .id(id)
+//                .start();
+//    }
     }
 }
--- app/src/main/java/hotramen/apps/projectcs/MainActivity.java
+++ app/src/main/java/hotramen/apps/projectcs/MainActivity.java
@@ -2,16 +2,118 @@ package hotramen.apps.projectcs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+import io.realm.Realm;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    @ViewById
+    EditText etUsername;
+
+    @ViewById
+    EditText etPassword;
+
+    @ViewById
+    CheckBox cbRemember;
+
+
+    Realm realm = Realm.getDefaultInstance();
+
+
+
+    @AfterViews
+    public void init() {
+        SharedPreferences sharedPreferences = getSharedPreferences("login_info", MODE_PRIVATE);
+        boolean isRemembered = sharedPreferences.getBoolean("isRemembered", false);
+        String userID = sharedPreferences.getString("uuid", null);
+
+        User userRemembered = realm.where(User.class).equalTo("uuid", userID).findFirst();
+
+        if (isRemembered){
+            assert userRemembered != null;
+            etUsername.setText(userRemembered.getName());
+            etPassword.setText(userRemembered.getPassword());
+            cbRemember.setChecked(true);
+        }
+
+
+
     }
+
+    @Click(R.id.btnRegister)
+    public void register(){
+        Register_.intent(this)
+                .start();
+    }
+
+
+    @Click(R.id.btnSignIn)
+    public void login(){
+        SharedPreferences sharedPreferences = getSharedPreferences("login_info", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+//        String u = sharedPreferences.getString("username", null);
+//        String p = sharedPreferences.getString("password", null);
+
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+
+        long result = realm.where(User.class)
+                .equalTo("name", username)
+                .count();
+//        if(u == null && TextUtils.isEmpty(user) && TextUtils.isEmpty(pass)){
+
+        User userSignIn = realm.where(User.class)
+                .equalTo("name", username)
+                .findFirst();
+
+
+        if(result == 0){
+            Toast.makeText(getApplicationContext(), "User does not exist", Toast.LENGTH_LONG).show() ;
+        }
+        else {
+            assert userSignIn != null;
+
+            if(!userSignIn.getPassword().equals(password)){
+                Toast.makeText(getApplicationContext(), "Invalid Password", Toast.LENGTH_LONG).show() ;
+            }
+
+            else{
+                Home_.intent(this)
+                        .start();
+                editor.putBoolean("isRemembered", cbRemember.isChecked());
+                editor.putString("uuid", userSignIn.getUuid());
+                editor.apply();
+
+            }
+        }
+    }
+
+
+//    @Click(R.id.btnClear)
+//    public void clear(){
+//        SharedPreferences sharedPreferences = getSharedPreferences("login_info", MODE_PRIVATE);
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//        editor.clear();
+//        editor.apply();
+//
+//        etUsername.setText("");
+//        etPassword.setText("");
+//        cbRemember.setChecked(false);
+//
+//        Toast.makeText(getApplicationContext(), "Preferences Cleared", Toast.LENGTH_LONG).show();
+//    }
+
 }
